7be884147e301d3f986c7d30985cd016e8c1e88a
Solium Dev <lucas.develop.prog@gmail.com>
2023-03-23 00:40:28 -0300

    :tada: login routes added to main routes file

--- src/lib/routes/index.ts
+++ src/lib/routes/index.ts
@@ -8,8 +8,10 @@ import { fastifyAuth } from "@fastify/auth";
 
 import { FastifyInstance } from "fastify";
 import { indexController } from "../../controllers/index";
+import { loginRoutes } from "./login";
 
 export const routes = async function (app: FastifyInstance) {
   app.get("/", indexController);
   userRoutes(app);
+  loginRoutes(app);
 };
